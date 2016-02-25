c894380da5e6161b8fa8351bdd316dd834a0288d
Aaron Silber <aaron@burlingtonbytes.com>
2016-02-25 14:51:05 -0500

    re-add social icons, header background, reducing redundant styles

--- wp-content/themes/wruv/header.php
+++ wp-content/themes/wruv/header.php
@@ -186,6 +186,192 @@ wp_nav_menu(array(
 ));
 ?>
 
+				<?php
+				if (of_get_option('social_header', '1') == '1') {
+				?>
+						<div class="header-social">
+							 <ul id="footer-social">
+
+				<?php
+				if (of_get_option('facebook') != "") {
+				?>
+									<li class="facebook footer-social"><a href="<?php
+					echo of_get_option('facebook', 'no entry');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('twitter') != "") {
+				?>
+									<li class="twitter footer-social"><a href="<?php
+					echo of_get_option('twitter');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('digg') != "") {
+				?>
+									<li class="digg footer-social"><a href="<?php
+					echo of_get_option('digg');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('youtube') != "") {
+				?>
+									<li class="youtube footer-social"><a href="<?php
+					echo of_get_option('youtube');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('vimeo') != "") {
+				?>
+									<li class="vimeo footer-social"><a href="<?php
+					echo of_get_option('vimeo');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('rss') != "") {
+				?>
+									<li class="rss footer-social"><a href="<?php
+					echo of_get_option('rss');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('flickr') != "") {
+				?>
+									<li class="flickr1 footer-social"><a href="<?php
+					echo of_get_option('flickr');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('lastfm') != "") {
+				?>
+									<li class="lastfm footer-social"><a href="<?php
+					echo of_get_option('lastfm');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('pinterest') != "") {
+				?>
+									<li class="pinterest footer-social"><a href="<?php
+					echo of_get_option('pinterest');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('vk') != "") {
+				?>
+									<li class="vk footer-social"><a href="<?php
+					echo of_get_option('vk');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('google') != "") {
+				?>
+									<li class="google footer-social"><a href="<?php
+					echo of_get_option('google');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('amazon') != "") {
+				?>
+									<li class="amazon footer-social"><a href="<?php
+					echo of_get_option('amazon');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('beatport') != "") {
+				?>
+									<li class="beatport footer-social"><a href="<?php
+					echo of_get_option('beatport');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('myspace') != "") {
+				?>
+									<li class="myspace footer-social"><a href="<?php
+					echo of_get_option('myspace');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('instagram') != "") {
+				?>
+									<li class="instagram footer-social"><a href="<?php
+					echo of_get_option('instagram');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('mixcloud') != "") {
+				?>
+									<li class="mixcloud footer-social"><a href="<?php
+					echo of_get_option('mixcloud');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('soundcloud') != "") {
+				?>
+									<li class="soundcloud footer-social"><a href="<?php
+					echo of_get_option('soundcloud');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('resident') != "") {
+				?>
+									<li class="resident footer-social"><a href="<?php
+					echo of_get_option('resident');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+				<?php
+				if (of_get_option('tumblr') != "") {
+				?>
+									<li class="tumblr footer-social"><a href="<?php
+					echo of_get_option('tumblr');
+				?>" target="_blank"></a></li><?php
+				}
+				?>
+
+			 </ul>
+	</div><!-- end .header-social -->
+	<?php
+	}
+	?>
+
+ </div><!-- end #main -->
+</div><!-- end #header -->
+
       </div><!-- end .main-navigation -->
    </div>
 <!-- Wrap -->

7b51e8d3c972953a8a9f4d641136e57274b5dfd1
Brian Bittman <bbittman@burlingtonbytes.com>
2016-07-05 16:40:24 -0400

    hotfixes to live (including add ga snippet)

--- wp-content/themes/wruv/header.php
+++ wp-content/themes/wruv/header.php
@@ -71,7 +71,16 @@ error_reporting(E_ALL);
 
 <!-- Wordpress functions -->
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-12936262-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 </head>
 
